44f77afedec25dc57a1d3aca13fa51bfcf9e67fb
kgrowkgrow <kevingrow11@gmail.com>
2021-01-14 17:56:31 -0800

    working on auth

--- src/App.js
+++ src/App.js
@@ -2,6 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  handleLogin = (info) => {
+    console.log('login')
+    this.handleAuthFetch(info, 'http://localhost:3000/login' )
+  }
+
+  handleSignup = (info) => {
+    console.log('sign up')
+    this.handleAuthFetch(info, 'http://localhost:3000/users')
+  }
+
+  handleAuthFetch = (info, request) => {
+    fetch(request, {
+      method: 'POST',
+      headers: {
+        'Content-Type': "application/json"
+      },
+      body: JSON.stringify({
+        name: info.name,
+        password: info.password
+      })
+    })
+    .then(resp => resp.json())
+    .then(data => {
+      this.setState({user: data.user, token: data.token}, () => {
+        this.props.history.push('/')
+      })
+
+    })
+  }
   return (
     <div className="App">
       <header className="App-header">
